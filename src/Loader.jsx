816214fa9a9bbd347b47e6b515a1a6fdf0ac6407
Binatari Akene <akenebina@gmail.com>
2023-08-07 15:39:00 +0100

    add player

--- src/Loader.jsx
+++ src/Loader.jsx
@@ -1,11 +1,12 @@
-
+import Lottie from "lottie-react";
+import bomb from "./bomb.json";
 import React from "react";
 
 const Loader = () => {
   return (
     <div>
       <div className=' w-full bg-black h-screen flex flex-col justify-center text-center items-center'>
-        <img src='/boom.gif' />
+      <Lottie animationData={bomb} loop={true} />
         <p className=' text-[30px] font-medium text-[#df5722]'>Loading</p>
       </div>
     </div>
